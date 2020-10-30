7a629d6be65e7ad24619e848e0c3af12a0ca2084
timlucmiptev <info@timlucmiptev.space>
2020-10-30 11:28:54 +0200

    integrate local startup scripts

--- local-start.sh
+++ local-start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 BTC_DATADIR=/Volumes/sandisk/BTC
 ELECTRS_DATADIR=/Volumes/sandisk/electrs
-ELECTRS_HOST=127.0.0.1
-ELECTRS_PORT=50001
-PROXY_PORT=50002
+export ELECTRS_HOST=127.0.0.1
+export ELECTRS_PORT=50001
+export PROXY_PORT=50002
 
-# bitcoind -datadir=$BTC_DATADIR
-# node src/server.js
+bitcoind -datadir=$BTC_DATADIR &
+node src/server.js &
 
 ./electrs/target/release/electrs \
     -vvvv  --timestamp \
